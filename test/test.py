47c2f63b6a05a1a7e52e85b37dbc65f8f93161a1
leeexing <lixingyaha@163.com>
2018-05-11 08:34:03 +0800

    <docs> add test.py to mock some data

--- test/test.py
+++ test/test.py
@@ -4,6 +4,10 @@
 提供一些测试数据
 """
 import app.util.connector import get_redis_connection
+import json, re
+from string import ascii_letters
+
+STRING_SELECT = ascii_letters + '0123456789'
 
 R = get_redis_connection()
 
