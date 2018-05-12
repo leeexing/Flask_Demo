64abcbc10ddf96be4af849f87a3bd1d68c9aa5ad
leeexing <lixingyaha@163.com>
2018-05-12 15:53:14 +0800

    <docs> update test.py

--- test/test.py
+++ test/test.py
@@ -4,12 +4,18 @@
 提供一些测试数据
 """
 import app.util.connector import get_redis_connection
-import json, re
+import json, time
 from string import ascii_letters
 
 STRING_SELECT = ascii_letters + '0123456789'
 
 R = get_redis_connection()
+
+def generate_data():
+    """生成数据"""
+    obj = {
+        'time': time.now()
+    }
 
 def main():
     pass
